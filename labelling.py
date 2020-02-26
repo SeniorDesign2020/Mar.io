d4e1fb95c67623cfbd67227391e61c5c1b16eb25
sarthakbabbar <sarthakbabbar.07@gmail.com>
2020-02-26 18:19:09 -0500

    improved the labelling and started working on another method for labelling when the original frames are zipped

--- labelling.py
+++ labelling.py
@@ -5,12 +5,14 @@ from Overlay import done
 import cv2
 from PIL import Image
 from tqdm import tqdm
-filename =  input('video_name\n')
+import shutil
+from copy import deepcopy
+video_name =  input('video_name\n')
 folder_name = input('folder_name\n')
 keys = []
 bucket = 'senior-design33'
-a="b"
-b="c"
+
+
 destination = 'LabelledFrames'
 if os.path.isfile('aws.txt'):
     with open('aws.txt','r') as in_file:
@@ -21,57 +23,38 @@ if os.path.isfile('aws.txt'):
         line=line.strip()
         SECRET_KEY = line
 else:
-    ACCESS_KEY = input('acces_key\n')
+    ACCESS_KEY = input('access_key\n')
     keys.append(ACCESS_KEY)
-    SECRET_KEY = input('secrete_key\n')
+    SECRET_KEY = input('secret_key\n')
     keys.append(SECRET_KEY)
     with open('aws.txt','w') as out_file:
         for line in keys:
             out_file.write(line)
             out_file.write("\n")
 
-s3_client = boto3.client('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
+os.mkdir('{}'.format(folder_name))
 
+s3_client = boto3.client('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
 paginator = s3_client.get_paginator('list_objects')
 operation_parameters = {'Bucket': bucket,
-                        'Prefix': 'Frames/{}/{}'.format(filename,folder_name)}
+                        'Prefix': 'Frames/{}/{}'.format(video_name, folder_name)}
 pages = paginator.paginate(**operation_parameters)
-for page in tqdm(pages):
+
+for page in pages:
     for obj in tqdm(page['Contents']):
         frame_number = obj['Key'].split('/')[-1]
-        try:
-            s3_client.download_file('senior-design33',obj['Key'],'frame.jpg')
-            
-            filtered_image = fil('frame.jpg')
-            image_cv2 = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2RGB)
-            image_pil = Image.fromarray(image_cv2)
-            #cv2.imwrite('fil_image.jpg',filtered_image)
-            image_label=done(image_pil)
-            image_label.save('labelled_image.jpg')
-            #cv2.imwrite('fil_image.jpg',filtered_image)
-            #labelled_image.save("labelled_image.jpg")
-            s3_client.upload_file('labelled_image.jpg',bucket,'LabelledFrames/{}/{}/{}'.format(filename,folder_name,frame_number))
-            os.remove("frame.jpg")
-            os.remove("labelled_image.jpg")
-        except:
-            s3_client.delete_object(Bucket= bucket,Key=obj['Key'])
-
-            
+        s3_client.download_file('senior-design33',obj['Key'],'frame.jpg')
         
+        filtered_image = fil('frame.jpg')
+        image_cv2 = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2RGB)
+        image_pil = Image.fromarray(image_cv2)
         
-'''
-os.mkdir('frames')    
-bucket = 'senior-design33'
-client = boto3.client('s3', aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
-response = client.list_objects_v2(
-            Bucket=bucket,
-            Prefix ='Frames/{}'.format(filename),
-            MaxKeys=100 )
+        image_label=done(image_pil)
+        image_label.save('{}/{}'.format(folder_name,frame_number))
+        
 
-for i,obj in enumerate(response['Contents']):
-    frame_number = obj['Key'].split('/')[-1]
-    
-    client.download_file(bucket, obj['Key'], 'frames/{}'.format(frame_number))
-
-'''
-
+shutil.make_archive('{}'.format(folder_name),'zip','{}'.format(folder_name))
+print('Uploading...')
+s3_client.upload_file('{}.zip'.format(folder_name), bucket, 'LabelledFrames/{}/{}.zip'.format(video_name,folder_name))    
+shutil.rmtree('{}'.format(folder_name))
+os.remove('{}.zip'.format(folder_name))        
